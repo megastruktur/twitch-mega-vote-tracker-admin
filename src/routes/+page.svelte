21da8a0232586bf5d2728123a983a71d9d0f0dbe
megastruktur <astromortis@gmail.com>
2023-11-29 18:34:48 +0300

    Adds title page with games list

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,43 @@
-<!-- YOU CAN DELETE EVERYTHING IN THIS PAGE -->
+<script lang="ts">
+	import { pb } from "$lib/pocketbase";
+	import type { GamesResponse } from "$lib/pocketbase-types";
+	import { ProgressBar } from "@skeletonlabs/skeleton";
+	import type { UnsubscribeFunc } from "pocketbase";
+	import { onDestroy } from "svelte";
 
-<div class="container h-full mx-auto flex justify-center items-center">
-	<div class="space-y-5">
-		<h1 class="h1">Let's get cracking bones!</h1>
-		<p>Start by exploring:</p>
-		<ul>
-			<li><code class="code">/src/routes/+layout.svelte</code> - barebones layout</li>
-			<li><code class="code">/src/app.postcss</code> - app wide css</li>
-			<li>
-				<code class="code">/src/routes/+page.svelte</code> - this page, you can replace the contents
-			</li>
-		</ul>
-	</div>
-</div>
+	let games: GamesResponse[]
+
+	let unsubscribeGames: UnsubscribeFunc
+
+	async function getData() {
+		unsubscribeGames = await pb.collection("games").subscribe("*", loadGames)
+		await loadGames()
+	}
+
+	async function loadGames() {
+		games = await pb.collection("games").getFullList({
+			sort: "-votesTotal"
+		})
+	}
+
+	onDestroy(() => {
+		if (unsubscribeGames !== undefined) {
+			unsubscribeGames()
+		}
+	})
+</script>
+
+{#await getData()}
+	<ProgressBar />
+{:then}
+	{#if games !== undefined}
+		<div class="w-96 ml-3">
+			{#each games as game(game.id)}
+				<div class="flex justify-between border-b text-xl">
+					<span class="w-60 flex-wrap">{game.name}</span>
+					<span class="w-10">{game.votesTotal}</span>
+				</div>
+			{/each}
+		</div>
+	{/if}
+{/await}
